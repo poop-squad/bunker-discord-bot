e2661e68bf20d5e9efcaa50c80987143e4820de2
Angelo Guerrero <amcgdev@gmail.com>
2020-03-19 18:51:39 +0400

    - Added eslint - Added eslint config

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 const secrets = require('./secrets.json');
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+	if (msg.content === 'ping') {
+		msg.reply('Pong!');
+	}
 });
 
 client.login(secrets.token);
